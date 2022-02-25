dbaa177177494fabd5886715ace1ae4fae902e5f
Makerblaker <blake@blakemiller.ca>
2022-02-25 16:07:31 -0500

    Added month and day check

--- on.py
+++ on.py
@@ -1,8 +1,21 @@
 import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+import datetime
 
-port = 5000
-s.connect(('localhost', port))
-sendString = "PROGRAM:ON"
-s.send(sendString.encode('ascii'))
-s.close()
+todayMonth = datetime.date.today().month
+todayDay = datetime.date.today().day
+
+if todayMonth == 11:
+    if todayDay > 12:
+        startLights()
+elif todayMonth == 12:
+    startLights()
+
+
+def startLights():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    port = 5000
+    s.connect(('localhost', port))
+    sendString = "PROGRAM:ON"
+    s.send(sendString.encode('ascii'))
+    s.close()
+    print("The lights are on")
