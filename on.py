e74ee610ecb2797c2e709bc9c0e61d5030581cdf
MakerBlaker <blake@blaskemiller.ca>
2022-11-12 21:02:48 -0500

    Moved function up.

--- on.py
+++ on.py
@@ -1,6 +1,14 @@
 import socket
 import datetime
 
+def startLights():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    port = 5000
+    s.connect(('localhost', port))
+    sendString = "PROGRAM:ON"
+    s.send(sendString.encode('ascii'))
+    s.close()
+
 todayMonth = datetime.date.today().month
 todayDay = datetime.date.today().day
 
@@ -12,13 +20,3 @@ elif todayMonth == 12:
 elif todayMonth == 1:
     if todayDay == 1:
         startLights()
-
-
-def startLights():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    port = 5000
-    s.connect(('localhost', port))
-    sendString = "PROGRAM:ON"
-    s.send(sendString.encode('ascii'))
-    s.close()
-    print("The lights are on")
